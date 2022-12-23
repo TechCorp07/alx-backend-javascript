e224fc43d3823f29b3a7231c9abbec62cb51efb4
Einstein <TechCorp07@users.noreply.github.com>
2022-12-24 01:01:55 +0200

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,26 +1,41 @@
 export default class Car {
   constructor(brand, motor, color) {
-    // Create objs
     this._brand = brand;
     this._motor = motor;
-    this._color = color;
+    this.color = color;
   }
 
-  // Methods
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
 
   cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
   }
-
-  // Setters
-
-  // Getters
 }
-/* class TestCar extends Car {};
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-console.log(tc1 instanceof TestCar);
-console.log(tc2 instanceof TestCar);
-console.log(tc1 == tc2); */
