15e5865054727bc565bba297a49846014bb5b1c0
Einstein <TechCorp07@users.noreply.github.com>
2022-12-24 01:00:14 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,22 @@
 import Car from './10-car';
 
-export default class EVCar extends Car {
+export default class EvCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  // eslint-disable-next-line
-	cloneCar() {
-    return new Car[Symbol.species]();
+  set range(value) {
+    this._range = value;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }
